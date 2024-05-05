655359e963fc27964cb084f4d467c4b3e0e6fbe3
Victor Caproni <victorcaproni04@gmail.com>
2024-05-05 17:29:01 -0300

    ClockInForm working

--- app/src/main/java/com/example/puctime/main/ClockInFormActivity.kt
+++ app/src/main/java/com/example/puctime/main/ClockInFormActivity.kt
@@ -1,7 +1,12 @@
 package com.example.puctime.main
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isEmpty
 import com.akexorcist.snaptimepicker.SnapTimePickerDialog
 import com.example.puctime.R
 import com.example.puctime.databinding.ActivityClockInFormBinding
@@ -16,18 +21,64 @@ class ClockInFormActivity : AppCompatActivity() {
         binding = ActivityClockInFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val defaultTimePicker = binding.defaultTimePicker
-        val customTimePicker = binding.customTimePicker
+        val checkInTime = binding.checkInTimeBox
+        val checkOutTime = binding.checkOutTimeBox
+        showPickedTimes(checkInTime, checkOutTime)
 
-        defaultTimePicker.setOnClickListener {
-            // Default TimePicker
+        val saveAppointmentButton = binding.saveClockInButton
+
+        saveAppointmentButton.setOnClickListener {
+
+            val checkInTimeText = binding.checkInSelectedTime.text.toString()
+            val checkOutTimetext = binding.checkOutSelectedTime.text.toString()
+            val clockInNametext = binding.clockInNameText.text.toString()
+            val dayOfTheWeekSelectedText = binding.weekDaysActv.text.toString()
+
+            if (clockInNametext.isEmpty()) {
+                binding.clockInInputLayout.error = "Dê um nome à seu compromisso!"
+            }
+
+            if (dayOfTheWeekSelectedText.isEmpty()) {
+                binding.weekDaysInputLayout.error = "Selecione um Dia da Semana!"
+            }
+
+            if (
+                checkInTimeText.isNotEmpty() &&
+                checkOutTimetext.isNotEmpty() &&
+                clockInNametext.isNotEmpty() &&
+                dayOfTheWeekSelectedText.isNotEmpty()
+            ) {
+                Log.d("clockInFormData", checkInTimeText)
+                Log.d("clockInFormData", checkOutTimetext)
+                Log.d("clockInFormData", clockInNametext)
+                Log.d("clockInFormData", dayOfTheWeekSelectedText)
+
+                Toast.makeText(this, "Apontamento criado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Erro ao criar apontamento", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val weekDays = resources.getStringArray(R.array.week_days)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_week_days_item, weekDays)
+        binding.weekDaysActv.setAdapter(arrayAdapter)
+    }
+
+    private fun showPickedTimes(
+        checkInTime: LinearLayout,
+        checkOutTime: LinearLayout
+    ) {
+        checkInTime.setOnClickListener {
             SnapTimePickerDialog.Builder().apply {
                 setTitle(R.string.title)
                 setTitleColor(android.R.color.white)
             }.build().apply {
-                setListener {
-                        hour, minute ->
-                    onTimePicked(hour, minute)
+                setListener { hour, minute ->
+                    onTimePickedForCheckIn(hour, minute)
                 }
             }.show(
                 supportFragmentManager,
@@ -35,37 +86,51 @@ class ClockInFormActivity : AppCompatActivity() {
             )
         }
 
-        customTimePicker.setOnClickListener {
+        checkOutTime.setOnClickListener {
             SnapTimePickerDialog.Builder().apply {
                 setTitle(R.string.title)
-                setTitleColor(android.R.color.black)
+                setTitleColor(android.R.color.white)
             }.build().apply {
-                setListener {
-                        hour, minute ->
-                    onTimePicked(hour, minute)
+                setListener { hour, minute ->
+                    onTimePickedForCheckOut(hour, minute)
                 }
             }.show(
                 supportFragmentManager,
                 SnapTimePickerDialog.TAG
             )
         }
-
     }
 
-    private fun onTimePicked(selectedHour: Int, selectedMinute: Int) {
+    private fun onTimePickedForCheckIn(selectedHour: Int, selectedMinute: Int) {
 
-        val selectedTime = binding.selectedTime
-
+        val checkInSelectedTime = binding.checkInSelectedTime
         val hour = selectedHour.toString()
             .padStart(2, '0')
         val minute = selectedMinute.toString()
             .padStart(2, '0')
-        selectedTime.text = String.format(
+        val selectedTime = String.format(
             getString(
                 R.string.selected_time_format,
                 hour, minute
             )
         )
+        checkInSelectedTime.text = selectedTime
+    }
+
+    private fun onTimePickedForCheckOut(selectedHour: Int, selectedMinute: Int) {
+
+        val checkOutSelectedTime = binding.checkOutSelectedTime
+        val hour = selectedHour.toString()
+            .padStart(2, '0')
+        val minute = selectedMinute.toString()
+            .padStart(2, '0')
+        val selectedTime = String.format(
+            getString(
+                R.string.selected_time_format,
+                hour, minute
+            )
+        )
+        checkOutSelectedTime.text = selectedTime
     }
 }
 
