875ad4699869a42b4d510e943c263ddb643f3771
Victor Caproni <victorcaproni04@gmail.com>
2024-05-11 20:37:52 -0300

    Validacao de Localizacao Funcionando

--- app/src/main/java/com/example/puctime/ui/fragments/CalendarOverviewFragment.kt
+++ app/src/main/java/com/example/puctime/ui/fragments/CalendarOverviewFragment.kt
@@ -1,6 +1,8 @@
 package com.example.puctime.ui.fragments
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,17 +11,21 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.puctime.R
 import com.example.puctime.databinding.FragmentCalendarOverviewBinding
+import com.example.puctime.models.Clockin
 import com.example.puctime.ui.adapter.DailyClockInAdapter
+import com.example.puctime.ui.interfaces.LocationValidationCallback
 import com.example.puctime.ui.interfaces.OnItemClickListener
 import com.example.puctime.ui.main.ClockInFormActivity
-import com.example.puctime.models.Clockin
 import com.example.puctime.viewmodel.ClockinViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
 
@@ -29,6 +35,8 @@ class CalendarOverviewFragment : Fragment() {
     private var myAdapter: DailyClockInAdapter? = null
     private var sheetDialog: BottomSheetDialog? = null
     private lateinit var progressView: ProgressBar
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private var _binding: FragmentCalendarOverviewBinding? = null
     private val binding get() = _binding!!
@@ -76,6 +84,84 @@ class CalendarOverviewFragment : Fragment() {
         }
     }
 
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+
+            requestPermissions(
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1000
+            )
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!(requestCode == 1000 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            Toast.makeText(requireContext(), "Permissão negada", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getDistance(latitude: Double, longitude: Double, latitudeArea: Double, longitudeArea: Double) : Float{
+        val results = FloatArray(1)
+        Location.distanceBetween(latitude, longitude, latitudeArea, longitudeArea, results)
+        return results[0]
+    }
+
+    private fun checkIfUserIsInValidLocation(latitude: Double, longitude: Double) : Boolean{
+        val latitudeArea = -22.79939738395089
+        val longitudeArea = -47.03968986299806
+
+        val distance = getDistance(latitude, longitude, latitudeArea, longitudeArea)
+        return distance <= 1000f
+
+        //VH's House Coordenates
+        // -22.79939738395089
+        // -47.03968986299806
+
+        //PUC CAMPINAS COORDENATES:
+        //-22.835194046406773
+        //-47.049244372907616
+    }
+
+
+
+    private fun validateLocation(callback: LocationValidationCallback) {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        val isValid = checkIfUserIsInValidLocation(location.latitude, location.longitude)
+                        callback.onLocationValidation(isValid)
+                    } ?: Toast.makeText(requireContext(), "Location not found", Toast.LENGTH_SHORT)
+                        .show()
+                }
+        }
+    }
+
     fun showBottomSheetDialog(clockin: Clockin) {
 
         sheetDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetStyle)
@@ -87,7 +173,8 @@ class CalendarOverviewFragment : Fragment() {
 
                 view = LayoutInflater.from(requireContext()).inflate(
                     R.layout.bottom_sheet_layout_clockin,
-                    (ConstraintLayout(requireContext())).findViewById(R.id.bottom_sheet_clockin))
+                    (ConstraintLayout(requireContext())).findViewById(R.id.bottom_sheet_clockin)
+                )
 
                 val clockInButton = view.findViewById<MaterialButton>(R.id.register_clockin_button)
                 clockInButton.setOnClickListener {
@@ -96,20 +183,25 @@ class CalendarOverviewFragment : Fragment() {
                 }
 
             }
+
             "aberto" -> {
                 view = LayoutInflater.from(requireContext()).inflate(
                     R.layout.bottom_sheet_layout_clockout,
-                    (ConstraintLayout(requireContext())).findViewById(R.id.bottom_sheet_clockout))
+                    (ConstraintLayout(requireContext())).findViewById(R.id.bottom_sheet_clockout)
+                )
 
-                val clockOutButton = view.findViewById<MaterialButton>(R.id.register_clockout_button)
+                val clockOutButton =
+                    view.findViewById<MaterialButton>(R.id.register_clockout_button)
                 clockOutButton.setOnClickListener {
                     registerClockOut(clockin)
                 }
             }
+
             else -> {
                 view = LayoutInflater.from(requireContext()).inflate(
                     R.layout.bottom_sheet_layout_already_registered,
-                    (ConstraintLayout(requireContext())).findViewById(R.id.bottom_sheet_already_registered))
+                    (ConstraintLayout(requireContext())).findViewById(R.id.bottom_sheet_already_registered)
+                )
             }
         }
 
@@ -118,35 +210,52 @@ class CalendarOverviewFragment : Fragment() {
     }
 
     private fun registerClockIn(clockin: Clockin) {
-        val repositoryAnswer = viewModel.saveClockInRegister(clockin)
 
-        Log.i("RegistroClockin", "Fragment: $repositoryAnswer")
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        checkLocationPermission()
+        validateLocation(object : LocationValidationCallback{
+            override fun onLocationValidation(isValid: Boolean) {
+                if(isValid){
+                    val repositoryAnswer = viewModel.saveClockInRegister(clockin)
 
-        when (repositoryAnswer) {
-            "true" -> {
-                progressView.visibility = View.GONE
-                Toast.makeText(
-                    requireContext(),
-                    "Entrada registrada com sucesso!",
-                    Toast.LENGTH_SHORT
-                ).show()
-                dismissBottomSheetDialog()
+                    Log.i("RegistroClockin", "Fragment: $repositoryAnswer")
+
+                    when (repositoryAnswer) {
+                        "true" -> {
+                            progressView.visibility = View.GONE
+                            Toast.makeText(
+                                requireContext(),
+                                "Entrada registrada com sucesso!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            dismissBottomSheetDialog()
+                        }
+
+                        "Fora de horário" -> {
+                            progressView.visibility = View.GONE
+                            Toast.makeText(requireContext(), "Fora de Horário", Toast.LENGTH_SHORT).show()
+                        }
+
+                        else -> {
+                            progressView.visibility = View.GONE
+                            Toast.makeText(requireContext(), "Erro ao registrada entrada!", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                } else {
+                    progressView.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Você está fora da PUC-Campinas", Toast.LENGTH_SHORT).show()
+                }
             }
+        })
 
-            "Fora de horário" -> {
-                progressView.visibility = View.GONE
-                Toast.makeText(requireContext(), "Fora de Horário", Toast.LENGTH_SHORT).show()
-            }
 
-            else -> {
-                progressView.visibility = View.GONE
-                Toast.makeText(requireContext(), "Erro ao registrada entrada!", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
     }
 
-    private fun registerClockOut(clockin: Clockin){
+    private fun registerClockOut(clockin: Clockin) {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        checkLocationPermission()
 
         val repositoryAnswer = viewModel.saveClockOutRegister(clockin)
 
---
+++ app/src/main/java/com/example/puctime/ui/interfaces/LocationValidationCallback.kt
@@ -0,0 +1,5 @@
+package com.example.puctime.ui.interfaces
+
+interface LocationValidationCallback {
+    fun onLocationValidation(isValid: Boolean)
+}
